f3a4c61b1f8166ce40974692e56e463f3251aa70
Darkest-White <darkest.white.fox@gmail.com>
2023-08-14 18:27:50 +0300

    Update: Sprite can move

--- TopDownShooter-SFML/Main.cpp
+++ TopDownShooter-SFML/Main.cpp
@@ -4,11 +4,24 @@ using namespace std;
 using namespace sf;
 
 
+int win_width = 1280;
+int win_height = 720;
 Event ev;
 
 int main()
 {
-	RenderWindow win(VideoMode(1280, 720), "TopDownShooter");
+	RenderWindow win(VideoMode(win_width, win_height), "TopDownShooter");
+
+	Image hero_image;
+	hero_image.loadFromFile("img/hero.png");
+
+	Texture hero_texture;
+	hero_texture.loadFromImage(hero_image);
+
+	Sprite hero_sprite;
+	hero_sprite.setTexture(hero_texture);
+	hero_sprite.setTextureRect(IntRect(0, 0, 313, 207));
+	hero_sprite.setPosition(win_width / 2, win_height / 2);
 
 	while (win.isOpen())
 	{
@@ -20,8 +33,13 @@ int main()
 			}
 		}
 
+		if (Keyboard::isKeyPressed(Keyboard::Up)) { hero_sprite.move(0, -1); }
+		if (Keyboard::isKeyPressed(Keyboard::Down)) { hero_sprite.move(0, 1); }
+		if (Keyboard::isKeyPressed(Keyboard::Left)) { hero_sprite.move(-1, 0); }
+		if (Keyboard::isKeyPressed(Keyboard::Right)) { hero_sprite.move(1, 0); }
+
 		win.clear();
-		//win.draw();
+		win.draw(hero_sprite);
 		win.display();
 	}
 }
