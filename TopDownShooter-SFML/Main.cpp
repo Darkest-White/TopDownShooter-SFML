6d08b3efa38d3a054c3f622520767416a11bd0ae
Darkest-White <darkest.white.fox@gmail.com>
2023-08-16 19:46:03 +0300

    Update

--- TopDownShooter-SFML/Main.cpp
+++ TopDownShooter-SFML/Main.cpp
@@ -14,13 +14,18 @@ int main()
 
 	Image hero_image;
 	hero_image.loadFromFile("img/hero.png");
+	Image hero_image_2;
+	hero_image_2.loadFromFile("img/hero_2.png");
 
 	Texture hero_texture;
 	hero_texture.loadFromImage(hero_image);
+	Texture hero_texture_2;
+	hero_texture_2.loadFromImage(hero_image_2);
 
 	Sprite hero_sprite;
 	hero_sprite.setTexture(hero_texture);
 	hero_sprite.setTextureRect(IntRect(0, 0, 313, 207));
+	//hero_sprite.setOrigin(313 / 2, 207 / 2);
 	hero_sprite.setPosition(win_width / 2, win_height / 2);
 
 	while (win.isOpen())
@@ -33,10 +38,31 @@ int main()
 			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Up)) { hero_sprite.move(0, -1); }
-		if (Keyboard::isKeyPressed(Keyboard::Down)) { hero_sprite.move(0, 1); }
-		if (Keyboard::isKeyPressed(Keyboard::Left)) { hero_sprite.move(-1, 0); }
-		if (Keyboard::isKeyPressed(Keyboard::Right)) { hero_sprite.move(1, 0); }
+		if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))
+		{
+			hero_sprite.move(0, -0.1);
+			hero_sprite.setTexture(hero_texture_2);
+			hero_sprite.setTextureRect(IntRect(0, 313, 207, -313));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))
+		{
+			hero_sprite.move(0, 0.1);
+			hero_sprite.setTexture(hero_texture_2);
+			hero_sprite.setTextureRect(IntRect(0, 0, 207, 313));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
+		{
+			hero_sprite.move(-0.1, 0);
+			hero_sprite.setTexture(hero_texture);
+			hero_sprite.setTextureRect(IntRect(313, 0, -313, 207));
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
+		{
+			hero_sprite.move(0.1, 0);
+			hero_sprite.setTexture(hero_texture);
+			hero_sprite.setTextureRect(IntRect(0, 0, 313, 207));
+		}
+
 
 		win.clear();
 		win.draw(hero_sprite);
