6e2f64bd5a31c9404059e240318e799dda7e0977
Darkest-White <darkest.white.fox@gmail.com>
2023-08-31 07:55:19 +0300

    Update

--- TopDownShooter-SFML/Main.cpp
+++ TopDownShooter-SFML/Main.cpp
@@ -11,6 +11,50 @@ Clock clk;
 float current_frame = 0;
 
 
+class Player
+{
+private:
+
+public:
+
+	float x, y, w, h, speed = 0;
+	int dir = 0;
+	String file;
+	Image image;
+	Texture texture;
+	Sprite sprite;
+
+	Player(String f, int x, int y, float w, float h)
+	{
+		file = f;
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+
+		image.loadFromFile("img/" + f);
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(0, 0, w, h));
+	}
+
+	void update(float time)
+	{
+		switch (dir)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+};
+
+
 int main()
 {
 	RenderWindow win(VideoMode(win_width, win_height), "TopDownShooter");
@@ -21,21 +65,18 @@ int main()
 	
 	float hero_speed = 0.5;
 
-	Image hero_image_1;
-	hero_image_1.loadFromFile("img/hero/hero_1.png");
-	Image hero_image_2;
-	hero_image_2.loadFromFile("img/hero/hero_2.png");
+	Image hero_image;
+	hero_image.loadFromFile("img/hero/hero.png");
 
-	Texture hero_texture_1;
-	hero_texture_1.loadFromImage(hero_image_1);
-	Texture hero_texture_2;
-	hero_texture_2.loadFromImage(hero_image_2);
+	Texture hero_texture;
+	hero_texture.loadFromImage(hero_image);
 
 	Sprite hero_sprite;
-	hero_sprite.setTexture(hero_texture_1);
+	hero_sprite.setTexture(hero_texture);
 	hero_sprite.setTextureRect(IntRect(0, 0, 313, 207));
 	hero_sprite.setOrigin(313 / 2, 207 / 2);
 	hero_sprite.setPosition(win_width / 2, win_height / 2);
+
 
 	while (win.isOpen())
 	{
@@ -49,33 +90,22 @@ int main()
 
 		if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))
 		{
-			current_frame += 0.005 * time;
-			if (current_frame > 3) current_frame = 0;
-
-			hero_sprite.setTexture(hero_texture_2);
-			hero_sprite.setTextureRect(IntRect(int(current_frame), 0, 207, 313));
-			hero_sprite.setOrigin(207 / 2, 313 / 2);
+			hero_sprite.setRotation(-90);
 			hero_sprite.move(0, -hero_speed * time);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))
 		{
-			hero_sprite.setTexture(hero_texture_2);
-			hero_sprite.setTextureRect(IntRect(207, 313, -207, -313));
-			hero_sprite.setOrigin(207 / 2, 313 / 2);
+			hero_sprite.setRotation(90);
 			hero_sprite.move(0, hero_speed * time);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
 		{
-			hero_sprite.setTexture(hero_texture_1);
-			hero_sprite.setTextureRect(IntRect(313, 207, -313, -207));
-			hero_sprite.setOrigin(313 / 2, 207 / 2);
+			hero_sprite.setRotation(180);
 			hero_sprite.move(-hero_speed * time, 0);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
 		{
-			hero_sprite.setTexture(hero_texture_1);
-			hero_sprite.setTextureRect(IntRect(0, 0, 313, 207));
-			hero_sprite.setOrigin(313 / 2, 207 / 2);
+			hero_sprite.setRotation(0);
 			hero_sprite.move(hero_speed * time, 0);
 		}
 
